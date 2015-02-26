c6ff687a1a8b0c3b1f188151368593e5fa988a0e
jdgarvey <jon.d.garvey@gmail.com>
2015-02-26 13:23:33 -0700

    Reformatting to be consistent with the book

--- app.js
+++ app.js
@@ -34,9 +34,33 @@ myModule.service('AngelloModel', function () {
             {name: 'Spike'}
         ],
         stories = [
-            {title: 'First story', description: 'Our first story.', criteria: 'Criteria pending.', status: 'To Do', type: 'Feature', reporter: 'Lukas Ruebbelke', assignee: 'Brian Ford'},
-            {title: 'Second story', description: 'Do something.', criteria: 'Criteria pending.', status: 'Back Log', type: 'Feature', reporter: 'Lukas Ruebbelke', assignee: 'Brian Ford'},
-            {title: 'Another story', description: 'Just one more.', criteria: 'Criteria pending.', status: 'Code Review', type: 'Enhancement', reporter: 'Lukas Ruebbelke', assignee: 'Brian Ford'}
+            {
+                title: 'First story',
+                description: 'Our first story.',
+                criteria: 'Criteria pending.',
+                status: 'To Do',
+                type: 'Feature',
+                reporter: 'Lukas Ruebbelke',
+                assignee: 'Brian Ford'
+            },
+            {
+                title: 'Second story',
+                description: 'Do something.',
+                criteria: 'Criteria pending.',
+                status: 'Back Log',
+                type: 'Feature',
+                reporter: 'Lukas Ruebbelke',
+                assignee: 'Brian Ford'
+            },
+            {
+                title: 'Another story',
+                description: 'Just one more.',
+                criteria: 'Criteria pending.',
+                status: 'Code Review',
+                type: 'Enhancement',
+                reporter: 'Lukas Ruebbelke',
+                assignee: 'Brian Ford'
+            }
         ];
 
     service.getStatuses = function () {
@@ -68,7 +92,15 @@ myModule.controller('MainCtrl', function (AngelloModel, AngelloHelper) {
     };
 
     main.createStory = function () {
-        main.stories.push({title: 'New Story', description: 'Description pending.', criteria: 'Criteria pending.', status: 'Back Log', type: 'Feature', reporter: 'Pending', assignee: 'Pending'});
+        main.stories.push({
+            title: 'New Story',
+            description: 'Description pending.',
+            criteria: 'Criteria pending.',
+            status: 'Back Log',
+            type: 'Feature',
+            reporter: 'Pending',
+            assignee: 'Pending'
+        });
     };
 
     main.setCurrentStatus = function (status) {
